bb2d8f2825ce4b92500473fafd3ef993bcf3435d
gunsal15 <gunsal15@ku.edu.tr>
2019-05-01 19:02:10 +0300

    added thread creation and join

--- p2.cpp
+++ p2.cpp
@@ -77,6 +77,7 @@ int pthread_sleep (int seconds)
 
 int main(int argc, char* argv[])
 {
+
   pthread_t thread_N;
   pthread_t thread_E;
   pthread_t thread_S;
@@ -84,19 +85,32 @@ int main(int argc, char* argv[])
 
   pthread_t thread_PO;
 
-  struct Car araba;
+  long t1 = 0, t2 = 1, t3 = 2, t4 = 3;
 
   for(int i = 1; i < argc; i++) { /* argv[0] is the program name */
-  if(strcmp(argv[i], "-s") == 0) {
-    simulationTime = atoi(argv[i+1]);
-  } else if(strcmp(argv[i], "-p") == 0) {
-    p = (double) atof(argv[i+1]);
+    if(strcmp(argv[i], "-s") == 0) {
+      simulationTime = atoi(argv[i+1]);
+    } else if(strcmp(argv[i], "-p") == 0) {
+      p = (double) atof(argv[i+1]);
+    }
   }
-}
 
-printf("simulation time: %d\n",simulationTime);
-printf("probability: %f\n",p);
+  printf("simulation time: %d\n",simulationTime);
+  printf("probability: %f\n",p);
 
+
+  pthread_create(&thread_N, &attr, section_thread_function, (void *)t1);
+  pthread_create(&thread_E, &attr, section_thread_function, (void *)t2);
+  pthread_create(&thread_S, &attr, section_thread_function, (void *)t3);
+  pthread_create(&thread_W, &attr, section_thread_function, (void *)t4);
+  pthread_create(&thread_PO, &attr, center_thread_function, NULL);
+
+  pthread_join(thread_N, NULL);
+  pthread_join(thread_E, NULL);
+  pthread_join(thread_S, NULL);
+  pthread_join(thread_W, NULL);
+  cout << "will join thread center" << endl;
+  pthread_join(thread_PO, NULL);
 
 
 }
